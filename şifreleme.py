7c083b3a945a469c6b289657346f8b822de72277
zarganax <54045618+zarganax@users.noreply.github.com>
2019-08-12 18:51:17 -0400

    Create şifreleme.py

---
+++ şifreleme.py
@@ -0,0 +1,11 @@
+print("Programa hoşgeldin.")
+sifre=input("Lütfen parolayı giriniz.\n")
+sifre=int(sifre)
+
+if sifre==123: 
+	print("Parolayı doğru girdiniz.")
+	input("Bilgilere erişmek için herhangi bir tuşa basınız.")
+	print("BJK1903 isimli modemin şifresi: ilhan2005")
+
+else:
+	print("Şifreyi yanlış girdiniz.")
